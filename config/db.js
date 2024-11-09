13e3451cc36bfc8a18ed710f3d823309efe65a42
MurugananthamB <murugananthamanand01212@gmail.com>
2024-11-10 01:09:19 +0530

    backend

--- config/db.js
+++ config/db.js
@@ -3,11 +3,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
-    process.exit(1);
+    process.exit(1); // Exit the process if MongoDB connection fails
   }
 };
 
